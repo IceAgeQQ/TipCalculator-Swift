aa81ac39021fd3c2e64401c9c8c6e221791c7199
IceAgeQQ <d830300042@gmail.com>
2015-05-18 21:27:32 -0700

    second commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Chao Xu on 15/5/18.
+//  Copyright (c) 2015年 Chao Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var totalTextFidld : UITextField!
+    @IBOutlet var taxPctSlider : UISlider!
+    @IBOutlet var taxPctLabel : UILabel!
+    @IBOutlet var resultsTextView : UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func calculateTapped(sender : AnyObject) {
+        tipCalc.total = Double((totalTextFidld.text as NSString).doubleValue)
+        
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        
+        for (tipPct, tipValue) in possibleTips {
+            results += "\(tipPct)%: \(tipValue)\n"
+        }
+        resultsTextView.text = results
+    }
+    @IBAction func taxPercentageChanged(sender : AnyObject) {
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
+    }
+    @IBAction func viewTapped(sender : AnyObject) {
+        totalTextFidld.resignFirstResponder()
+    }
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI(){
+        
+        totalTextFidld.text = String(format: "%0.2F", tipCalc.total)
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%) "
+        resultsTextView.text = ""
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
